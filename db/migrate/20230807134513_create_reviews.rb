224ab26e60fbe12aaacf146df433a34576885ee3
sayaotake <saya42438@gmail.com>
2023-09-11 01:20:37 +0900

    Webサービス　DBと接続

--- db/migrate/20230807134513_create_reviews.rb
+++ db/migrate/20230807134513_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
       t.string :content
-      t.references :user, null: false, foreign_key: true
+      t.references :, null: false, foreign_key: true
       t.references :photo, null: false, foreign_key: true
 
       t.timestamps
